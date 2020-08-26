8f7654289eac0caa182429b012e4956321b9fad2
bpisonic <barbara.pisonic@gmail.com>
2020-08-26 20:01:53 +0200

    Add unity project

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 4;
+    float rotationSpeed = 80;
+    float rotation = 0f;
+    float gravity = 8;
+
+    Vector3 moveDir = Vector3.zero; // => (0,0,0,); //move direction
+
+    CharacterController controller;
+    Animator anim; 
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+        anim = GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.isGrounded)  //ako je na povrsini
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("condition", 1);
+                moveDir = new Vector3 (0, 0, 1);
+              //moveDir = moveDir * speed;
+                moveDir *= speed;
+                moveDir = transform.TransformDirection(moveDir);
+            }
+            else //kada prestanemo drzati w
+            {
+                anim.SetInteger("condition", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+        }
+        rotation += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; //horizontal => a & d
+        transform.eulerAngles = new Vector3 (0, rotation, 0);
+
+        moveDir.y -= gravity * Time.deltaTime; // y=> up/down
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float speed = 30f;
+    //public bool canMove = false;
+    public bool canMove = false;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRot = Camera.main.transform.eulerAngles.x;
+        
+        Quaternion rotMin = Quaternion.Euler(new Vector3(-20f, 0, 0));
+        Quaternion rotMax = Quaternion.Euler(new Vector3(20f, 0, 0));
+        //Quaternion rotation = transform.rotation;
+        Quaternion rotation = Camera.main.transform.rotation;
+
+ 
+        if(!canMove){
+            if(rotation.x <= rotMin.x){            
+                canMove = true;
+                Debug.Log("Go");
+            }
+        }
+         
+        if(canMove){
+            if(rotation.x >= rotMax.x){
+                canMove = false;
+                Debug.Log("Stop");
+            }
+        }
+
+        if(canMove){
+            //this.gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            Vector3 cameraAngle = Camera.main.transform.forward;
+            this.gameObject.transform.Translate(cameraAngle * Time.deltaTime * speed);
+            Debug.Log("Walking"); 
+        }
+       
+        // if(Input.GetButtonDown("Fire1")){
+        //     canMove = !canMove;
+        // }
+
+       
+    }
+
+// OnCollisionEnter() -> sto ce se dogoditi nakon "sudara" OnCollisionStay i OnCollisionExit
+    private void OnCollisionEnter(Collision otherObject) 
+    {
+        if(otherObject.transform.tag == "stopper"){
+            canMove = false;  
+        } 
+
+        Debug.Log("Collision detected: " +  otherObject.gameObject.name);
+        
+    }
+}
